dc4de9ed88d4c93c63e492783c0bca6f340bd6d5
Olivier Pleindoux <pleindoux.olivier@gmail.com>
2020-05-27 12:36:17 +0200

    add validates rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  RATES = (1...5)
 
-  validates :rating, inclusion: { in: RATES }
+  RATES = [0, 1, 2, 3, 4, 5]
+  # validates :rating, :inclusion => 0..5
+  validates :rating, inclusion: { in: RATES }, numericality: { only_integer: true }
+
   validates :content, :rating, presence: true
-
 end
