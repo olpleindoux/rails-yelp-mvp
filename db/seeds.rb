b37310f3659ff00aa2926784fddfed8465ca89bc
Olivier Pleindoux <pleindoux.olivier@gmail.com>
2020-05-27 15:47:25 +0200

    add reviews on restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "belgian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+burger_home =  { name: "Burger home", address: "123 av de la liberté, 13127 Vitrolles", category: "french" }
+kebab =  { name: "Kebab des Merveilles", address: "2 place Pie, 84000 Avignon", category: "belgian" }
+muraille_de_chine =  { name: "La Muraille de Chine", address: "245 place de la République, 85134 Bolynes", category: "chinese" }
+
+[dishoom, pizza_east, burger_home, kebab, muraille_de_chine].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
